d87556f9503f126e4bdc263be303fbdf45b1e023
Josh Seltzer <jselt88@gmail.com>
2021-02-27 16:35:58 -0500

    fragile
    breaks with function requested from course to make form usable. undef error due to function implementation for grabbing props from form and injecting into url for featured-teacher

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 //App components
 import Home from './Home';
@@ -7,19 +7,25 @@ import About from './About';
 import Header from './Header';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
+import Featured from "./Featured";
 
 const App = () => (
     <BrowserRouter>
         <div className="container">
             <Header/>
-            <Route exact path="/">
-                <Home/>
-            </Route>
-            <Route path="/about" render={() => <About title='About'/>} />
-            <Route exact path="/teachers">
-                <Teachers/>
-            </Route>
-            <Route path="/courses" component={Courses} />
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" render={() => <About title='About'/>}/>
+                <Route exact path="/teachers">
+                    <Teachers/>
+                </Route>
+                <Route path="/teachers/:topic/:fname-:lname" component={Featured} />
+                <Route path="/courses" component={Courses}/>
+                <Route>
+                    <NotFound/>
+                </Route>
+            </Switch>
         </div>
     </BrowserRouter>
 );
